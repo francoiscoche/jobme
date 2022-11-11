c2e361488d3d19deadab7ec64fbdfbe73f8675b3
Francois <francois.coche@outlook.it>
2022-11-11 17:14:25 +0100

    Registration (Templating and debug)

--- app/src/Form/RegistrationFormType.php
+++ app/src/Form/RegistrationFormType.php
@@ -4,37 +4,88 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('address')
-            ->add('phone')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+            ->add('email', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Email',
+                    'class' => 'form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
                 ],
             ])
+            ->add('firstname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Firstname',
+                    'class' => 'form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Lastname',
+                    'class' => 'form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+                ],
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+                ],
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices' => [
+                    'Diventa un jobber (Offri i tuoi servizi)' => 'ROLE_JOBBER',
+                    'Inserisci un annuncio (Chiama un jobber)' => 'ROLE_RICHIEDENTE'
+
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Address',
+                    'class' => 'form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+                ],
+            ])
+            ->add('phone', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Phone',
+                    'class' => 'form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+                ],
+            ])
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'You should agree to our terms.',
+            //         ]),
+            //     ],
+            // ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => false,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password',
+                'placeholder' => 'Password',
+                'class' => 'form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -47,7 +98,26 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
+                ],
+                'label' => 'Let\'s Go!'
+            ]);
         ;
+
+
+        // data transformer
+        $builder->get('roles')
+                    ->addModelTransformer(new CallbackTransformer(
+                        function($rolesArray) {
+                            return count($rolesArray) ? $rolesArray[0] : null;
+                        },
+                        function ($rolesString) {
+                            return [$rolesString];
+                        }
+                    ));
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
